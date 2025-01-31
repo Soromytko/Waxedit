23257a3389a6da6c6f63d1cc602909a2d630937c
Soromytko <NikitaSoromytko@gmail.com>
2025-02-01 02:33:05 +0300

    Add raw text editing

--- src/App.cpp
+++ src/App.cpp
@@ -53,25 +53,19 @@ int App::run()
 	rendell_ui::WidgetSharedPtr rootWidget = std::make_shared<rendell_ui::Widget>();
 	rootWidget->setAnchor(rendell_ui::Anchor::centerStretch);
 
-	rendell_ui::Rectangle* rectangle = new rendell_ui::Rectangle(rootWidget.get());
-	rectangle->setColor(glm::vec4(1.0f, 0.7f, 0.5f, 1.0f));
-	rectangle->setMargins(30, 100, 20, 0);
-	rectangle->setAnchor(rendell_ui::Anchor::topStretch);
+	rendell_ui::TextEdit* textEdit = new rendell_ui::TextEdit(rootWidget.get());
+	textEdit->setSize(glm::vec2(50, 50));
+	textEdit->setMargins(0, 0, 0, 0);
+	textEdit->setAnchor(rendell_ui::Anchor::centerStretch);
+	textEdit->setText(LR"(#include <iostream>
 
-	rendell_ui::Rectangle* redRectangle = new rendell_ui::Rectangle(rectangle);
-	redRectangle->setColor(glm::vec4(1.0f, 0.0f, 0.0f, 0.0f));
-	redRectangle->setSize(glm::vec2(50.0f, 50.0f));
-	redRectangle->setAnchor(rendell_ui::Anchor::rightBottom);
-	redRectangle->setMargins(0, 10, 20, 0);
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+	)");
 
-	rendell_ui::Text* text = new rendell_ui::Text(rootWidget.get());
-	text->setText(L"Hello World!");
-	text->setFontPath("../res/Fonts/mononoki/mononoki-Regular.ttf");
-	text->setFontSize(glm::vec2(FONT_WIDTH, FONT_HEIGHT));
-	text->setBackgroundColor(glm::vec4(BACKGROUND_COLOR, BACKGROUND_COLOR, BACKGROUND_COLOR, 1.0f));
-	text->setColor(glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
-	text->setOffset(glm::vec2(-100.0f, -100.0f));
-	text->setAnchor(rendell_ui::Anchor::center);
 
 	_canvas->addWidget(rootWidget);
 
